72c79e15b8a6861a3581ea5f6870b7745d665d14
Markus Achtelik <markus.achtelik@mavt.ethz.ch>
2012-02-18 21:13:24 +0100

    mav_joy version ready for test

--- mav_joy/src/joy.cpp
+++ mav_joy/src/joy.cpp
@@ -17,10 +17,26 @@ Joy::Joy() :
   ros::NodeHandle nh;
   ros::NodeHandle pnh("~");
 
-  ptam_cmd_pub_ = nh.advertise<std_msgs::String> ("vslam/key_pressed", 10);
+  ptam_cmd_pub_ = nh_.advertise<std_msgs::String> ("vslam/key_pressed", 10);
 
-  joy_sub_ = nh.subscribe("joy", 10, &Joy::joyCallback, this);
-  pose_sub_ = nh.subscribe("pose", 10, &Joy::poseCallback, this);
+  ctrl_pub_ = nh_.advertise<asctec_hl_comm::mav_ctrl> ("fcu/control", 10);
+
+  mav_ctrl_.header.seq = 1;
+  mav_ctrl_.type = asctec_hl_comm::mav_ctrl::velocity;
+  mav_ctrl_.v_max_xy = 0;
+  mav_ctrl_.v_max_z = 0;
+  mav_ctrl_.x = 0;
+  mav_ctrl_.y = 0;
+  mav_ctrl_.z = 0;
+  mav_ctrl_.yaw = 0;
+
+  joy_sub_ = nh_.subscribe("joy", 10, &Joy::joyCallback, this);
+  pose_sub_ = nh_.subscribe("fcu/current_pose", 10, &Joy::poseCallback, this);
+
+  pnh.param("v_max_xy", v_max_.xy, 1.0); // m/s
+  pnh.param("v_max_z", v_max_.z, 1.0); // m/s
+  pnh.param("v_max_yaw", v_max_.yaw, 45.0); // deg/s
+  v_max_.yaw *= (M_PI / 180.0);
 
   // defaults for Logitech Wireless Gamepad F710
   pnh.param("channel_x_", axes_.x, 3);
@@ -29,21 +45,45 @@ Joy::Joy() :
   pnh.param("channel_yaw_", axes_.yaw, 0);
   pnh.param("channel_takeoff_", buttons_.takeoff, 9);
   pnh.param("channel_ctrl_enable_", buttons_.ctrl_enable, 4);
+  pnh.param("channel_ctrl_enable_", buttons_.function_enable, 5);
   pnh.param("channel_ptam_reset_", buttons_.ptam_reset, 2);
   pnh.param("channel_ptam_space_", buttons_.ptam_space, 3);
   pnh.param("channel_ptam_auto_init_", buttons_.ptam_auto_init, 1);
   pnh.param("channel_filter_init_", buttons_.filter_init, 0);
+
+  pnh.param("direction_x", axes_direction_.x, 1);
+  pnh.param("direction_y", axes_direction_.y, 1);
+  pnh.param("direction_z", axes_direction_.z, 1);
+  pnh.param("direction_yaw", axes_direction_.yaw, 1);
 
   button_handler_.registerCallback(buttons_.takeoff, &Joy::takeoffCallback, this);
   button_handler_.registerCallback(buttons_.filter_init, &Joy::filterInitCallback, this);
   button_handler_.registerCallback(buttons_.ptam_space, &Joy::ptamSpaceCallback, this);
   button_handler_.registerCallback(buttons_.ptam_reset, &Joy::ptamResetCallback, this);
   button_handler_.registerCallback(buttons_.ptam_auto_init, &Joy::ptamAutoInitCallback, this);
+  button_handler_.registerCallback(buttons_.ctrl_enable, &Joy::ctrlEnableCallback, this);
 
+  namespace_ = nh.getNamespace();
+  if(namespace_ != "/"){
+    while(namespace_.at(0) == '/')
+      namespace_.erase(0,1);
+    namespace_ = "/" + namespace_;
+  }
+}
+
+bool Joy::controlEnabled()
+{
+  return current_joy_.buttons[buttons_.ctrl_enable] == 1;
+}
+
+bool Joy::functionsEnabled()
+{
+  return current_joy_.buttons[buttons_.function_enable] == 1;
 }
 
 void Joy::joyCallback(const sensor_msgs::JoyConstPtr & msg)
 {
+  current_joy_ = *msg;
 
   if (first_joy_callback_)
   {
@@ -51,13 +91,85 @@ void Joy::joyCallback(const sensor_msgs::JoyConstPtr & msg)
     first_joy_callback_ = false;
     return;
   }
-
   button_handler_.update(msg->buttons);
+
+  if (controlEnabled())
+  {
+    sendMavCommand(msg);
+    watchdog_timer_ = nh_.createTimer(ros::Duration(5), &Joy::watchdog, this, true, true);
+  }
+}
+
+bool Joy::sendMavCommand(const sensor_msgs::JoyConstPtr & msg)
+{
+
+  int size = msg->axes.size();
+  if (axes_.x >= size || axes_.y >= size || axes_.z >= size || axes_.yaw >= size)
+  {
+    ROS_ERROR("Axis index exceeds axes size: x:%d y:%d z:%d yaw:%d ", axes_.x, axes_.y, axes_.z, axes_.yaw);
+    return false;
+  }
+
+  const geometry_msgs::Quaternion & q = current_pose_.pose.orientation;
+  // get only yaw from the quaternion and rotate vx, vy from body to world coordinates, most likely not the most efficient way ...
+  float yaw = atan2(2.0 * (q.w * q.z + q.x * q.y), 1.0 - 2.0 * (q.y * q.y + q.z * q.z));
+  float s_yaw = sin(yaw);
+  float c_yaw = cos(yaw);
+  float vx_b = msg->axes[axes_.x];
+  float vy_b = msg->axes[axes_.y];
+
+  mav_ctrl_.header.seq++;
+  mav_ctrl_.header.stamp = msg->header.stamp;
+  mav_ctrl_.type = asctec_hl_comm::mav_ctrl::velocity;
+  mav_ctrl_.v_max_xy = v_max_.xy;
+  mav_ctrl_.v_max_z = v_max_.z;
+  mav_ctrl_.x = (c_yaw * vx_b - s_yaw * vy_b) * v_max_.xy * axes_direction_.x;
+  mav_ctrl_.y = (s_yaw * vx_b + c_yaw * vy_b) * v_max_.xy * axes_direction_.y;
+  mav_ctrl_.z = msg->axes[axes_.z] * v_max_.z * axes_direction_.z;
+  mav_ctrl_.yaw = msg->axes[axes_.yaw] * v_max_.yaw * axes_direction_.yaw;
+
+  ctrl_pub_.publish(mav_ctrl_);
+
+  return true;
+}
+
+void Joy::stopMav()
+{
+  mav_ctrl_.type = asctec_hl_comm::mav_ctrl::velocity;
+  mav_ctrl_.v_max_xy = 0;
+  mav_ctrl_.v_max_z = 0;
+  mav_ctrl_.x = 0;
+  mav_ctrl_.y = 0;
+  mav_ctrl_.z = 0;
+  mav_ctrl_.yaw = 0;
+
+  ros::Rate r(10);
+  for (int i = 0; i < 10; i++)
+  {
+    mav_ctrl_.header.seq++;
+    mav_ctrl_.header.stamp = ros::Time::now();
+    ctrl_pub_.publish(mav_ctrl_);
+    r.sleep();
+  }
+}
+
+void Joy::watchdog(const ros::TimerEvent & e){
+  stopMav();
+  ROS_WARN("Joystick watchdog triggered");
+}
+
+void Joy::ctrlEnableCallback(int val)
+{
+  if (val == 0)
+  {
+    watchdog_timer_.stop(); //everything went well, so we can stop the timer
+    stopMav();
+  }
 }
 
 void Joy::poseCallback(const geometry_msgs::PoseStampedConstPtr & msg)
 {
-  ;
+  current_pose_ = *msg;
 }
 
 void Joy::takeoffCallback(int val)
@@ -67,9 +179,9 @@ void Joy::takeoffCallback(int val)
 
 void Joy::filterInitCallback(int val)
 {
-  std::cout << "filterinit: " << val << std::endl;
-  if(val == 1){
-    system("rosrun dynamic_reconfigure dynparam set vismaggps filter_init true --timeout=2");
+  if (val == 1)
+  {
+    setDynParam("vismaggps_fusion init_filter true");
   }
 }
 
@@ -97,13 +209,21 @@ void Joy::ptamResetCallback(int val)
 
 void Joy::ptamAutoInitCallback(int val)
 {
-  std_msgs::StringPtr msg(new std_msgs::String);
   if (val == 1)
   {
-    msg->data = "a";
-    ptam_cmd_pub_.publish(msg);
-    ROS_INFO("Sent \"a\" to ptam");
+    ROS_WARN("Autoinit not implemented yet ;)");
+    // dynreconf call ...
   };
+}
+
+bool Joy::setDynParam(const std::string & param_string)
+{
+  int ret = system(std::string("rosrun dynamic_reconfigure dynparam set --timeout=2 " + param_string).c_str());
+
+  ROS_INFO_COND(ret == 0, "dynamic reconfigure call for %s succeeded", param_string.c_str());
+  ROS_WARN_COND(ret != 0, "dynamic reconfigure call for %s failed", param_string.c_str());
+
+  return ret == 0;
 }
 
 int main(int argc, char** argv)
--- mav_joy/src/joy.h
+++ mav_joy/src/joy.h
@@ -16,7 +16,15 @@
 
 #include "change_detector.h"
 
-struct AxisAssignment
+struct AxesAssignment
+{
+  int x;
+  int y;
+  int z;
+  int yaw;
+};
+
+struct AxesDirection
 {
   int x;
   int y;
@@ -28,10 +36,18 @@ struct ButtonAssignment
 {
   int takeoff;
   int ctrl_enable;
+  int function_enable;
   int ptam_reset;
   int ptam_space;
   int ptam_auto_init;
   int filter_init;
+};
+
+struct VMax
+{
+  double xy;
+  double z;
+  double yaw;
 };
 
 class Joy
@@ -39,24 +55,48 @@ class Joy
   typedef sensor_msgs::Joy::_buttons_type ButtonType;
 
 private:
+  ros::NodeHandle nh_;
   ros::Publisher ctrl_pub_;
   ros::Publisher ptam_cmd_pub_;
   ros::Subscriber joy_sub_;
   ros::Subscriber pose_sub_;
 
-  AxisAssignment axes_;
+  std::string namespace_;
+
+  AxesAssignment axes_;
+  AxesDirection axes_direction_;
   ButtonAssignment buttons_;
 
+  geometry_msgs::PoseStamped current_pose_;
+  asctec_hl_comm::mav_ctrl mav_ctrl_;
+  sensor_msgs::Joy current_joy_;
+
+  ros::Timer watchdog_timer_;
+
+  VMax v_max_;
+
   bool first_joy_callback_;
+
+  bool controlEnabled();
+  bool functionsEnabled();
+
+  bool setDynParam(const std::string & param_string);
+
+  bool sendMavCommand(const sensor_msgs::JoyConstPtr & msg);
+  void stopMav();
 
   void joyCallback(const sensor_msgs::JoyConstPtr & msg);
   void poseCallback(const geometry_msgs::PoseStampedConstPtr & msg);
 
   void takeoffCallback(int val);
+  void ctrlEnableCallback(int val);
   void filterInitCallback(int val);
   void ptamSpaceCallback(int val);
   void ptamResetCallback(int val);
   void ptamAutoInitCallback(int val);
+
+  void watchdog(const ros::TimerEvent & e);
+
 public:
   Joy();
   ChangeDetector<std::vector<int> > button_handler_;
