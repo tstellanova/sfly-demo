f30326c4a777820dd1013bee8dbd585a840b3d81
Markus Achtelik <markus.achtelik@mavt.ethz.ch>
2012-02-20 23:32:19 +0100

    josystick now sends body velocities, added commandconverter to imu launch file

--- mav_joy/src/joy.cpp
+++ mav_joy/src/joy.cpp
@@ -19,7 +19,7 @@ Joy::Joy() :
 
   ptam_cmd_pub_ = nh_.advertise<std_msgs::String> ("vslam/key_pressed", 10);
 
-  ctrl_pub_ = nh_.advertise<asctec_hl_comm::mav_ctrl> ("fcu/control", 10);
+  ctrl_pub_ = nh_.advertise<asctec_hl_comm::mav_ctrl> ("fcu/control_body", 10);
 
   mav_ctrl_.header.seq = 1;
   mav_ctrl_.type = asctec_hl_comm::mav_ctrl::velocity;
@@ -31,7 +31,7 @@ Joy::Joy() :
   mav_ctrl_.yaw = 0;
 
   joy_sub_ = nh_.subscribe("joy", 10, &Joy::joyCallback, this);
-  pose_sub_ = nh_.subscribe("fcu/current_pose", 10, &Joy::poseCallback, this);
+  //  pose_sub_ = nh_.subscribe("fcu/current_pose", 10, &Joy::poseCallback, this);
 
   pnh.param("v_max_xy", v_max_.xy, 1.0); // m/s
   pnh.param("v_max_z", v_max_.z, 1.0); // m/s
@@ -64,9 +64,10 @@ Joy::Joy() :
   button_handler_.registerCallback(buttons_.ctrl_enable, &Joy::ctrlEnableCallback, this);
 
   namespace_ = nh.getNamespace();
-  if(namespace_ != "/"){
-    while(namespace_.at(0) == '/')
-      namespace_.erase(0,1);
+  if (namespace_ != "/")
+  {
+    while (namespace_.at(0) == '/')
+      namespace_.erase(0, 1);
     namespace_ = "/" + namespace_;
   }
 }
@@ -110,21 +111,31 @@ bool Joy::sendMavCommand(const sensor_msgs::JoyConstPtr & msg)
     return false;
   }
 
-  const geometry_msgs::Quaternion & q = current_pose_.pose.orientation;
-  // get only yaw from the quaternion and rotate vx, vy from body to world coordinates, most likely not the most efficient way ...
-  float yaw = atan2(2.0 * (q.w * q.z + q.x * q.y), 1.0 - 2.0 * (q.y * q.y + q.z * q.z));
-  float s_yaw = sin(yaw);
-  float c_yaw = cos(yaw);
-  float vx_b = msg->axes[axes_.x];
-  float vy_b = msg->axes[axes_.y];
+  //  const geometry_msgs::Quaternion & q = current_pose_.pose.orientation;
+  //  // get only yaw from the quaternion and rotate vx, vy from body to world coordinates, most likely not the most efficient way ...
+  //  float yaw = atan2(2.0 * (q.w * q.z + q.x * q.y), 1.0 - 2.0 * (q.y * q.y + q.z * q.z));
+  //  float s_yaw = sin(yaw);
+  //  float c_yaw = cos(yaw);
+  //  float vx_b = msg->axes[axes_.x];
+  //  float vy_b = msg->axes[axes_.y];
+  //
+  //  mav_ctrl_.header.seq++;
+  //  mav_ctrl_.header.stamp = msg->header.stamp;
+  //  mav_ctrl_.type = asctec_hl_comm::mav_ctrl::velocity;
+  //  mav_ctrl_.v_max_xy = v_max_.xy;
+  //  mav_ctrl_.v_max_z = v_max_.z;
+  //  mav_ctrl_.x = (c_yaw * vx_b - s_yaw * vy_b) * v_max_.xy * axes_direction_.x;
+  //  mav_ctrl_.y = (s_yaw * vx_b + c_yaw * vy_b) * v_max_.xy * axes_direction_.y;
+  //  mav_ctrl_.z = msg->axes[axes_.z] * v_max_.z * axes_direction_.z;
+  //  mav_ctrl_.yaw = msg->axes[axes_.yaw] * v_max_.yaw * axes_direction_.yaw;
 
   mav_ctrl_.header.seq++;
-  mav_ctrl_.header.stamp = msg->header.stamp;
-  mav_ctrl_.type = asctec_hl_comm::mav_ctrl::velocity;
+  mav_ctrl_.header.stamp = ros::Time::now();//msg->header.stamp;
+  mav_ctrl_.type = asctec_hl_comm::mav_ctrl::velocity_body;
   mav_ctrl_.v_max_xy = v_max_.xy;
   mav_ctrl_.v_max_z = v_max_.z;
-  mav_ctrl_.x = (c_yaw * vx_b - s_yaw * vy_b) * v_max_.xy * axes_direction_.x;
-  mav_ctrl_.y = (s_yaw * vx_b + c_yaw * vy_b) * v_max_.xy * axes_direction_.y;
+  mav_ctrl_.x = msg->axes[axes_.x] * v_max_.xy * axes_direction_.x;
+  mav_ctrl_.y = msg->axes[axes_.y] * v_max_.xy * axes_direction_.y;
   mav_ctrl_.z = msg->axes[axes_.z] * v_max_.z * axes_direction_.z;
   mav_ctrl_.yaw = msg->axes[axes_.yaw] * v_max_.yaw * axes_direction_.yaw;
 
@@ -153,7 +164,8 @@ void Joy::stopMav()
   }
 }
 
-void Joy::watchdog(const ros::TimerEvent & e){
+void Joy::watchdog(const ros::TimerEvent & e)
+{
   stopMav();
   ROS_WARN("Joystick watchdog triggered");
 }
@@ -179,31 +191,40 @@ void Joy::takeoffCallback(int val)
 
 void Joy::filterInitCallback(int val)
 {
-  if (val == 1)
+  if (functionsEnabled())
   {
-    setDynParam("vismaggps_fusion init_filter true");
+    if (val == 1)
+    {
+      setDynParam("vismaggps_fusion init_filter true");
+    }
   }
 }
 
 void Joy::ptamSpaceCallback(int val)
 {
-  std_msgs::StringPtr msg(new std_msgs::String);
-  if (val == 1)
+  if (functionsEnabled())
   {
-    msg->data = "Space";
-    ptam_cmd_pub_.publish(msg);
-    ROS_INFO("Sent \"Space\" to ptam");
+    std_msgs::StringPtr msg(new std_msgs::String);
+    if (val == 1)
+    {
+      msg->data = "Space";
+      ptam_cmd_pub_.publish(msg);
+      ROS_INFO("Sent \"Space\" to ptam");
+    }
   }
 }
 
 void Joy::ptamResetCallback(int val)
 {
-  std_msgs::StringPtr msg(new std_msgs::String);
-  if (val == 1)
+  if (functionsEnabled())
   {
-    msg->data = "r";
-    ptam_cmd_pub_.publish(msg);
-    ROS_INFO("Sent \"r\" to ptam");
+    std_msgs::StringPtr msg(new std_msgs::String);
+    if (val == 1)
+    {
+      msg->data = "r";
+      ptam_cmd_pub_.publish(msg);
+      ROS_INFO("Sent \"r\" to ptam");
+    }
   }
 }
 
